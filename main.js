92179e6b3b2546ec8c6ee3c2a001b8d64ed5b6ed
hinagiku <holdstock@yeah.net>
2018-09-16 16:03:18 +0800

    main.js加logo

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1280, height: 720})
+  mainWindow = new BrowserWindow({width: 1280, height: 720,icon: '/logo.ico'})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
